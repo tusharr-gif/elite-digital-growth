80a4d6cc80174638e9f800a01ed8e757f987c5d5
tushar sonar <tusharpotdar16@gmail.com>
2026-03-01 11:43:33 +0530

    chore: update supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ptmigsefczfrvbhspzxq.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_z3OhltHnuJch7yWDPMacdg_1Qi_ZnRL';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0bWlnc2VmY3pmcnZiaHNwenhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNDUwNDEsImV4cCI6MjA4NzkyMTA0MX0.7KoMThXbTxVcXik9HyYpkkM0rHlP9r_jWkm8kM9Xczc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
